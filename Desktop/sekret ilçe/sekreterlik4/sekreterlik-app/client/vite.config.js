1ce03024d88cfd5bd5a3ec29b63e8e182e0425e8
murat kavum <muratkavum@gmail.com>
2025-11-02 19:31:46 +0300

    Remove unused PWA import, add complete Render build fix guide

--- Desktop/sekret ilçe/sekreterlik4/sekreterlik-app/client/vite.config.js
+++ Desktop/sekret ilçe/sekreterlik4/sekreterlik-app/client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+// PWA plugin geçici olarak devre dışı - @babel/traverse sorunu nedeniyle
+// import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
